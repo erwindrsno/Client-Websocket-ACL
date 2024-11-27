3ab11de44697b8db1a2bd7240f64666859f8f2f1
Erwin Darsono <erwindarsono08@gmail.com>
2024-11-27 16:16:12 +0700

    reconstruct receiving data from server

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+cd target
+java -jar ws-client-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/ws/client/EmptyClient.java
+++ src/main/java/ws/client/EmptyClient.java
@@ -10,6 +10,7 @@ import java.nio.file.StandardCopyOption;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
+import org.java_websocket.framing.PongFrame;
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.*;
 
@@ -32,6 +33,7 @@ public class EmptyClient extends WebSocketClient {
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         try {
+            logger.info("Connected to server : " + handshakedata.getHttpStatusMessage());
             fos = new FileOutputStream(sourcePath.toFile());
         } catch (Exception e) {
             e.printStackTrace();
@@ -39,49 +41,59 @@ public class EmptyClient extends WebSocketClient {
     }
 
     @Override
-    public void onMessage(String json) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            UserFile userFile = mapper.readValue(json, UserFile.class);
-            Path targetPath = Paths.get(userFile.getFilePath());
-            String user = userFile.getUser();
-
-            // file rename
-            //sourcePath is created beforehand
-            Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-
-
-            this.aclSetter = new Acl();
-            this.aclSetter.setRwxAcl(targetPath, user);
-            logger.info("ACL set");
-        } catch (Exception e) {
-            logger.info("error occured during setting ACL");
-            e.printStackTrace();
+    public void onMessage(String message) {
+        if(message.equals("PING")){
+            send("PONG");
         }
+        // logger.info(json);
+        // ObjectMapper mapper = new ObjectMapper();
+        // try {
+        //     UserFile userFile = mapper.readValue(json, UserFile.class);
+        //     Path targetPath = Paths.get(userFile.getFilePath());
+        //     String user = userFile.getUser();
+
+        //     // file rename
+        //     //sourcePath is created beforehand
+        //     Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+
+        //     this.aclSetter = new Acl();
+        //     this.aclSetter.setRwxAcl(targetPath, user);
+        //     logger.info("ACL set");
+        // } catch (Exception e) {
+        //     logger.info("error occured during setting ACL");
+        //     e.printStackTrace();
+        // }
     }
 
     @Override
     public void onMessage(ByteBuffer message) {
         boolean isLast = false;
-        try {
-            logger.info("Writing to file : " + message.remaining());
-            byte[] data = new byte[message.remaining()];
-            if (message.remaining() < 10240) {
-                isLast = true;
-            }
+        logger.info(message.toString());
+        // try {
+        //     logger.info("Writing to file : " + message.remaining());
+        //     byte[] data = new byte[message.remaining()];
+        //     if (message.remaining() < 10240) {
+        //         isLast = true;
+        //     }
 
-            message.get(data);
-            fos.write(data);
+        //     //4byte int
+        //     //ada protokol
+        //     //signature : 
+        //     // sha512, chunk size, hashing 
 
-            if (isLast) {
-                logger.info("File received");
-                logger.info("FOS closed");
-                fos.close();
-            }
-        } catch (Exception e) {
-            logger.error("Error occured during receiving file");
-            e.printStackTrace();
-        }
+        //     message.get(data);
+        //     fos.write(data);
+
+        //     if (isLast) {
+        //         logger.info("File received");
+        //         logger.info("FOS closed");
+        //         fos.close();
+        //     }
+        // } catch (Exception e) {
+        //     logger.error("Error occured during receiving file");
+        //     e.printStackTrace();
+        // }
     }
 
     // @Override
@@ -112,4 +124,9 @@ public class EmptyClient extends WebSocketClient {
     public void onError(Exception ex) {
         System.err.println("an error occurred:" + ex);
     }
+
+    // @Override
+    // public void onPing(ByteBuffer ping) {
+    //     System.out.println("pinged");
+    // }
 }
